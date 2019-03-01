baf9382029444891e9d0befce38efff0e656ce26
youjie <youjie@datastory.com.cn>
2019-03-01 11:46:30 +0800

    chore(构建): 增加banner

--- bin/ds.js
+++ bin/ds.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
-/**
- * 这种用法是为了防止操作系统用户没有将node装在默认的/usr/bin路径里。当系统看到这一行的时候
- * 首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作
- * */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}(function () { 'use strict';
+
+
+
+}));
--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,12 @@ export default {
   input: "src/main.ts",
   output: [
     {
+      banner: "#!/usr/bin/env node",
+      /**
+       * 头部
+       * 这种用法是为了防止操作系统用户没有将node装在默认的/usr/bin路径里。当系统看到这一行的时候
+       * 首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作
+       * */
       name: "ds",
       file: "bin/ds.js",
       format: "umd"
