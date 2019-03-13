1b9f0ad115d849252b4d0cc128b88a88ba075043
youjie <youjie@datastory.com.cn>
2019-03-13 14:30:42 +0800

    build(构建): 设置name字段并检测name是否合法

--- src/lib/init/options.ts
+++ src/lib/init/options.ts
@@ -8,6 +8,7 @@ const exists = require('fs').existsSync
 //检查你输入的 app-name 是否符合 npm 包名命名规范
 const validateName = require('validate-npm-package-name')
 
+import getGitUser from './git-user'
 
 // 注册handlebars的helper辅助函数
 Handlebars.registerHelper('if_eq', function (a, b, opts) {
@@ -22,8 +23,17 @@ return a === b
     : opts.fn(this)
 })
 
-export default function options(){
-
+export default function options(name,dir){
+    let opts=getMetadata(dir)
+    //设置name字段并检测name是否合法
+    setDefault(opts, 'name', name) 
+    setValidateName(opts) 
+    
+    // 获取 name 和 email，用于生成 package.json 里面的 author 字段
+    const author = getGitUser()
+    if (author) {
+        setDefault(opts, 'author', author)
+    }
 }
 
 /**
@@ -53,3 +63,45 @@ function getMetadata (dir) {
 
     return opts
 }
+
+/**
+ * 向meta配置对象中添加一下默认字段
+ *
+ * @param {Object} opts
+ * @param {String} key
+ * @param {String} val
+ */
+
+function setDefault (opts, key, val) {
+    if (opts.schema) {
+      opts.prompts = opts.schema
+      delete opts.schema
+    }
+    const prompts = opts.prompts || (opts.prompts = {})
+    if (!prompts[key] || typeof prompts[key] !== 'object') {
+      prompts[key] = {
+        'type': 'string',
+        'default': val
+      }
+    } else {
+      prompts[key]['default'] = val
+    }
+  }
+
+/** 
+ * 验证npm包名
+*/
+function setValidateName (opts) {
+    //获取包名name
+    const name = opts.prompts.name
+    const customValidate = name.validate
+    name.validate = name => {
+      const its = validateName(name)
+      if (!its.validForNewPackages) {
+        const errors = (its.errors || []).concat(its.warnings || [])
+        return 'Sorry, ' + errors.join(' and ') + '.'
+      }
+      if (typeof customValidate === 'function') return customValidate(name)
+      return true
+    }
+  }
