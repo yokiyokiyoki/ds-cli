17eb16ccbe1cc4a3396519fccb5253ccea88bd61
youjie <youjie@datastory.com.cn>
2019-03-18 11:34:49 +0800

    feat(本地模板): 逻辑判断

--- src/lib/init/cli-init.ts
+++ src/lib/init/cli-init.ts
@@ -2,9 +2,11 @@ const cmd = require('commander');
 const exists = require('fs').existsSync;
 const inquirer = require('inquirer');
 const path=require('path');
+import localPath from "./local-path"
 import logger from '../../utils/logger';
 import checkVersion from '../../utils/check-version';
 import downloadAndGenerate from "./downloadAndGenerate"
+import generate from './generate'
 
 export default function(...args) {
     const template = args[0];  // 模板名称
@@ -33,12 +35,25 @@ export default function(...args) {
 
 
     function run() {
-        checkVersion(()=> {
-            //例子ds init webpack testname
-            const gitTemplateUrl = `https://git.datatub.com:Uranus/ds-cli-${template}-template#master`
-            path.join(__dirname, '../')
-            downloadAndGenerate(gitTemplateUrl,name)
-        });
+        if(localPath.isLocalPath(template)){
+            //支持本地文件模板，如ds init /usr/webpack test
+            const templatePath=localPath.getTemplatePath(template)
+            if(path.exists(templatePath)){
+                generate(name, templatePath, to, err => {
+                    if (err) logger.fatal(err)
+                    console.log()
+                    logger.success(`生成${name}`)
+                })
+            }else{
+                logger.fatal(`本地模板${template}没有找到`)
+            }
+        }else{
+            checkVersion(()=> {
+                //例子ds init webpack testname
+                const gitTemplateUrl = `https://git.datatub.com:Uranus/ds-cli-${template}-template#master`
+                downloadAndGenerate(gitTemplateUrl,name)
+            });
+        }
     }
 }
 
