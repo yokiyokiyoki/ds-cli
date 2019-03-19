bf493ce2a73067dfcdb7c07854654a3dcf02c0a4
youjie <youjie@datastory.com.cn>
2019-03-19 14:53:54 +0800

    阶段性提交

--- src/lib/init/local-path.ts
+++ src/lib/init/local-path.ts
@@ -1,15 +1,15 @@
-const path=require('path');
+const path=require('path')
 
 export default {
-    isLocalPath(templatePath) {
-        // 判断是否是本地路径,uinx是以.或者/区分,windows是c:或者d:区分
-        return /^[./]|(^[a-zA-Z]:)/.test(templatePath);
+    isLocalPath (templatePath) {
+        //判断是否是本地路径,uinx是以.或者/区分;windows是c:或者d:区分
+        return /^[./]|(^[a-zA-Z]:)/.test(templatePath)
     },
 
-    getTemplatePath(templatePath) {
-        // 把路径化成绝对路径
+    getTemplatePath (templatePath) {
+        //把路径化成绝对路径
         return path.isAbsolute(templatePath)
         ? templatePath
-        : path.normalize(path.join(process.cwd(), templatePath));
+        : path.normalize(path.join(process.cwd(), templatePath))
     }
-};
+}
