fa55a7c65a35483803aba1bd4f9133e55ffec216
youjie <youjie@datastory.com.cn>
2019-03-22 17:51:31 +0800

    fix(获取仓库): 打印

--- src/lib/list/cli-list.ts
+++ src/lib/list/cli-list.ts
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 import tool from '../../utils/tool';
 
 export default async function(...args) {
-    console.log(logSymbols.info,chalk.green('正在努力写'));
+    
     // 获取仓库列表
     const res=await tool.request({
         url: 'https://api.github.com/users/yokiyokiyoki/repos',
@@ -13,12 +13,13 @@ export default async function(...args) {
     );
     let list;
     if(res.status===200) {
+        console.log(logSymbols.info,chalk.green('共有下列模板'))
         list=res.data.filter((item)=> {
             return item.name.includes('ds-cli')&&item.name.includes('template');
-        }).map((item)=> {
-            return item.name;
-        });
-        console.log(res.data,list);
+        }).forEach(item=>{
+            console.log()
+            console.log(chalk.green(item.name))
+        })
     } else {
         console.log(logSymbols.error,`获取仓库列表失败${res.data}`);
     }
